33701d66f05bded1d3e6e061244f5051b858b5ba
h6ker2969 <147671693+h6ker2969@users.noreply.github.com>
2023-11-13 16:36:25 -0500

    Create a screen

---
+++ screen.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+def screen_creater():
+    import pygame
+    import colors
+
+    pygame.init()
+
+
+    # Set up the screen
+    screen = pygame.display.set_mode((800, 600))
+    screen.fill(colors.RED)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    
